b62dfbafdfe4d131554fdba98d456a348ea223e0
“Sumire <wakamacchan443@gmail.com>
2021-06-09 21:17:39 +0900

    secound save

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,12 +5,33 @@ class UsersController < ApplicationController
         @user = User.new
     end
 
+    def new
+        @choru = Choru.new
+    end
+
+
     def edit
-        @choru = Choru.find(params[:id)
+        @choru = Choru.find(params[:id])
+    end
+    
+    def update
+        choru = Choru.find(params[:id])
+        if choru.update(choru_params)
+            redirect_to:action => "show", :id => choru.id
+        else
+            redirect_to:action => "new"
+        end
+
     end
 
     def show
         @user = User.find(params[:id]) 
+    end
+
+    def destroy
+        choru = Choru.find(params[:id])
+        choru.destroy
+        redirect_to action: :index
     end
     
 
