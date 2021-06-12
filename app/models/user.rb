3cc965ad3a720abe458ddf8ea666b5554768684f
“Sumire <wakamacchan443@gmail.com>
2021-06-13 00:19:04 +0900

    third save

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,18 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise :database_authenticatable, 
+         :registerable,
+         :confirmable,
+         :recoverable, 
+         :rememberable,
+         :trackable, 
+         :validatable
+       
+
 
          has_many :chorus, dependent: :destroy
          validates :name, presence: true
          validates :profile, length: { maximum: 200 }
-         mount_uploader :image, ImageUploader
-         
+         mount_uploader :image, ImageUploader         
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -49,7 +49,7 @@ Rails.application.configure do
 
   # Raise an error on page load if there are pending migrations.
   config.active_record.migration_error = :page_load
-
+  
   # Highlight code that triggered database queries in logs.
   config.active_record.verbose_query_logs = true
 
@@ -73,4 +73,5 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  
 end
